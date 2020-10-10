89a7a00c75c9c269300622e76f8355f5b103b1bd
thanos loupas <csd3222@csd.uoc.gr>
2020-10-10 15:50:04 +0300

    Game Initial Setup - Charactel Controller, AI , NavMesh (1st 2Hours of Course)

---
+++ Assets/1. Character & NPC Controller/Scripts/HeroController.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroController : MonoBehaviour
+{
+    Animator animator; // reference to the animator component
+    NavMeshAgent agent; // reference to the NavMeshAgent
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed",agent.velocity.magnitude);
+    }
+}
