a5a44e1a41490dd2b09b56e7ab2720f012fadddd
tpaslou <csd3222@csd.uoc.gr>
2020-10-12 20:32:11 +0300

    Scene Manager , Game Manager

---
+++ Assets/3. Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  //What level the game is currently in
+  //Methods to load and unload game levels
+  //keep track of the game state
+  //generate other persistent systems
+
+  private string _currentLevelName = string.Empty;
+
+  private List<AsyncOperation> _loadOperations;
+  private void Start()
+  {
+      DontDestroyOnLoad(gameObject);
+      _loadOperations=new List<AsyncOperation>();
+      LoadLevel("Main");
+  }
+
+  void OnLoadOperationComplete(AsyncOperation ao)
+  {
+      Debug.Log("Load Complete.");
+  } 
+  void OnUnloadOperationComplete(AsyncOperation ao)
+  {
+      if (_loadOperations.Contains(ao))
+      {
+          _loadOperations.Remove(ao);
+          //dispatch message
+          //tranasition between scenes
+      }
+      Debug.Log("Unload Complete.");
+      
+  }
+  public void LoadLevel(string levelName)
+  {
+      AsyncOperation ao=SceneManager.LoadSceneAsync(levelName,LoadSceneMode.Additive);
+      if (ao == null)
+      {
+          Debug.LogError("[GameManager]Unable to load level"+levelName);
+          return;
+      }
+      ao.completed += OnLoadOperationComplete;
+      _loadOperations.Add(ao);
+      _currentLevelName = levelName;
+  }
+
+  public void UnloadLevel(string levelName)
+  {
+      AsyncOperation ao=SceneManager.LoadSceneAsync(levelName);
+      if (ao == null)
+      {
+          Debug.LogError("[GameManager]Unable to unload level"+levelName);
+          return;
+      }
+      ao.completed += OnUnloadOperationComplete;
+  }
+}
--- Assets/Common/Scripts/MouseManager.cs
+++ Assets/Common/Scripts/MouseManager.cs
@@ -1,15 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
-    public LayerMask clickableLayer; // layermask used to isolate raycasts against clickable layers
+    public LayerMask clickableLayer;
 
-    public Texture2D pointer; // normal mouse pointer
-    public Texture2D target; // target mouse pointer
-    public Texture2D doorway; // doorway mouse pointer
-    
+    public Texture2D pointer;
+    public Texture2D target;
+    public Texture2D doorway;
+
     public EventVector3 OnClickEnvironment;
+
     void Update()
     {
         // Raycast into scene
@@ -27,27 +30,26 @@ public class MouseManager : MonoBehaviour
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
             }
 
+            // If environment surface is clicked, invoke callbacks.
             if (Input.GetMouseButtonDown(0))
             {
                 if (door)
                 {
                     Transform doorway = hit.collider.gameObject.transform;
-                    OnClickEnvironment.Invoke(doorway.position+doorway.forward *10);
+                    OnClickEnvironment.Invoke(doorway.position + doorway.forward * 10);
                 }
                 else
                 {
-                    OnClickEnvironment.Invoke(hit.point);    
+                    OnClickEnvironment.Invoke(hit.point);
                 }
-                
-                
             }
         }
         else
         {
-           
             Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
         }
     }
 }
+
 [System.Serializable]
-public class EventVector3 : UnityEvent<Vector3>{}
+public class EventVector3 : UnityEvent<Vector3> { }
